d85587de5176baa660098d634ad4cb6cb11c57f8
chb <gyqls234@gmail.com>
2023-12-18 19:04:28 +0900

    :sparkles: saveMember Test Successful

--- Backend/src/test/java/com/ontheblock/www/member/repository/MemberRepositoryTest.java
+++ Backend/src/test/java/com/ontheblock/www/member/repository/MemberRepositoryTest.java
@@ -1,16 +1,19 @@
 package com.ontheblock.www.member.repository;
 
+import com.ontheblock.www.OntheblockApplication;
 import com.ontheblock.www.member.domain.Member;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@DataJpaTest
-
+@SpringBootTest
+@ContextConfiguration(classes = OntheblockApplication.class)
 class MemberRepositoryTest {
 
     @Autowired
