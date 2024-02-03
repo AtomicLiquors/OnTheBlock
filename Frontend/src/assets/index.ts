bd3ca49ef12b5b2658db80b1c331249efd7f291e
chb <gyqls234@gmail.com>
2024-02-03 22:06:26 +0900

    Landing page image update

---
+++ Frontend/src/assets/index.ts
@@ -0,0 +1,25 @@
+import Kakao from "@/assets/login/kakao_login_button.png";
+import Google from "@/assets/login/google_login_button.png";
+import Naver from "@/assets/login/naver_login_button.png";
+import Banner from "@/assets/banners/landing.jpeg";
+
+
+import Logo_Large from "@/assets/logos/logo_large.png";
+import Logo_White_Large from "@/assets/logos/logo_white_large.png";
+import Logo_Default from "@/assets/logos/logo.png";
+import Logo_White from "@/assets/logos/logo_white.png";
+
+export const socialLoginBtn = {
+    Kakao: Kakao,
+    Google: Google,
+    Naver: Naver,
+}
+
+export { Banner};
+
+export const Logo = {
+    Large: Logo_Large,
+    WhiteLarge: Logo_White_Large,
+    Default: Logo_Default,
+    White: Logo_White,
+}
--- Frontend/src/pages/landing/Landing.tsx
+++ Frontend/src/pages/landing/Landing.tsx
@@ -2,12 +2,19 @@ import React from "react";
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 
-import kakaoBtn from "@/assets/kakao_login_button.png";
-import googleBtn from "@/assets/google_login_button.png"
+import { Logo, Banner, socialLoginBtn } from "@/assets";
 import Button from "react-bootstrap/Button";
-import Logo from "@/assets/logos/logo_large.png";
-import Banner from "@/assets/banners/landing.jpeg";
 
+const kakaoLoginUrl =
+  import.meta.env.VITE_REACT_APP_BACKEND + "ontheblock/api/kakao/login";
+const googleLoginUrl =
+  import.meta.env.VITE_REACT_APP_BACKEND + "ontheblock/api/google/login";
+const naverLoginUrl =
+  import.meta.env.VITE_REACT_APP_BACKEND + "ontheblock/api/naver/login";
+
+const handleLoginButtonClick = (url: string) => {
+    window.location.href = url;
+}
 
 const Landing: React.FC = () => {
   const navigate = useNavigate();
@@ -16,57 +23,46 @@ const Landing: React.FC = () => {
     <S.Wrap>
       <S.Card>
         <S.Banner>
-          <S.Logo src={Logo}></S.Logo>
-          <div style={{ textAlign: "center", color: "white", dropShadow: "10px 10px 10px black" }}>
-            나의 소소한 연주가, 색다른 합주로 태어나는 커뮤니티
+          <S.Logo src={Logo.Large}></S.Logo>
+          <S.Subtitle>
+            <span style={{fontSize: '1.2rem'}}>나의 소소한 연주가, 색다른 합주로 태어나는 커뮤니티</span>
             <br />
             다양한 뮤지션들을 만나보세요. 빅데이터로 관심 있는 연주를
             찾아보세요.
-          </div>
-
+          </S.Subtitle>
           <S.LoginContainer>
-            <div>
-              <a
-                href={
-                  import.meta.env.VITE_REACT_APP_BACKEND +
-                  "ontheblock/api/kakao/login"
-                }
-              >
-                <S.LoginImage src={kakaoBtn} />
-              </a>
-            </div>
-            <div>
-              <a
-                href={
-                  import.meta.env.VITE_REACT_APP_BACKEND +
-                  "ontheblock/api/google/login"
-                }
-              >
-                <S.LoginImage src={googleBtn} />
-              </a>
-            </div>
-            <div>
-            <a
-                href={
-                  import.meta.env.VITE_REACT_APP_BACKEND +
-                  "ontheblock/api/naver/login"
-                }
-              >
-                네이버 아이디 로그인
-              </a>
-            </div>
+            <S.LoginImage
+              onClick={() => handleLoginButtonClick(kakaoLoginUrl)}
+              src={socialLoginBtn.Kakao}
+            />
+
+            <S.LoginImage
+              onClick={() => handleLoginButtonClick(googleLoginUrl)}
+              src={socialLoginBtn.Google}
+            />
+
+            <S.LoginImage
+              onClick={() => handleLoginButtonClick(naverLoginUrl)}
+              src={socialLoginBtn.Naver}
+            />
           </S.LoginContainer>
         </S.Banner>
       </S.Card>
     </S.Wrap>
   );
-}
+};
 
 const S = {
   LoginContainer: styled.div`
-    > div {
-      margin-top: 10px;
-    }
+    display: flex;
+    flex-direction: column;
+    gap: 0.4rem;
+  `,
+
+  Subtitle: styled.div`
+    text-align: center;
+    color: white;
+    filter: drop-shadow(2px 2px 2px rgba(0, 0, 0, 0.8));
   `,
 
   Wrap: styled.div`
@@ -76,43 +72,47 @@ const S = {
     height: 100vh;
     background-color: #252525;
   `,
-  
+
   Card: styled.div`
     display: flex;
     flex-direction: column;
-    justify-content:center;
-    align-items:center;
-   `,
+    justify-content: center;
+    align-items: center;
+  `,
 
-   LoginImage : styled.img`
-      width :200px; 
-      cursor:pointer; 
-      padding: 0px;
-   `,
+  LoginImage: styled.img`
+    width: 200px;
+    cursor: pointer;
+    padding: 0px;
+  `,
 
-   NavigateButton : styled(Button)`
-     margin-top :20px; 
-     width :100%;   
-   `,
+  NavigateButton: styled(Button)`
+    margin-top: 20px;
+    width: 100%;
+  `,
 
-   Logo: styled.img`
-     width: 600px;
-   `,
+  Logo: styled.img`
+    width: 600px;
+  `,
 
-   Banner: styled.div`
+  Banner: styled.div`
     position: relative;
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    background-image: linear-gradient(to bottom, rgba(117, 85, 63, 0.3), rgba(156, 96, 100, 0.5)), url(${Banner});
+    background-image: linear-gradient(
+        to bottom,
+        rgba(117, 85, 63, 0.3),
+        rgba(156, 96, 100, 0.5)
+      ),
+      url(${Banner});
     background-size: cover;
     width: 100vw;
     height: 840px;
     gap: 100px;
     padding-top: 50px;
-   `,
+  `,
 };
 
 export default Landing;
-
