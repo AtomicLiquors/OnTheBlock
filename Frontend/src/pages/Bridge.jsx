0bc33d7be1535267c0df1f0dd25a9511d710e871
AtomicLiquors <95601940+AtomicLiquors@users.noreply.github.com>
2024-07-02 14:10:13 +0900

    :recycle: extracted parseCookie Method from Bridge.jsx

--- Frontend/src/pages/Bridge.jsx
+++ Frontend/src/pages/Bridge.jsx
@@ -6,21 +6,28 @@ function Bridge() {
   const location = useLocation(); // useLocation 훅을 사용하여 현재 경로 정보를 가져옴
   const queryParams = new URLSearchParams(location.search); // 쿼리 문자열을 파싱하기 위해 사용
 
+  const parseCookie = (cookieString) => {
+    //To-Do : cookie 형식이 맞는지 검증하고 아니면 에러처리?
+    
+    const cookies = cookieString.split(';').map((cookie) => {
+      const [name, value] = cookie.trim().split('=');
+      return { [name]: value.trim() };
+    });
+
+    const accessToken = cookies.find((cookie) => 'accessToken' in cookie)?.accessToken;
+    const refreshToken = cookies.find((cookie) => 'refreshToken' in cookie)?.refreshToken;
+    const memberId = cookies.find((cookie) => 'memberId' in cookie)?.memberId;
+
+    return [accessToken, refreshToken, memberId];
+  }
 
   useEffect(() => {
       // 쿠키를 불러옴
       const cookieString = document.cookie;
 
       // 쿠키에서 데이터를 분리한 후 데이터 저장
-      const cookies = cookieString.split(';').map((cookie) => {
-        const [name, value] = cookie.trim().split('=');
-        return { [name]: value.trim() };
-      });
-
-      const accessToken = cookies.find((cookie) => 'accessToken' in cookie)?.accessToken;
-      const refreshToken = cookies.find((cookie) => 'refreshToken' in cookie)?.refreshToken;
-      const memberId = cookies.find((cookie) => 'memberId' in cookie)?.memberId;
-    
+      const [accessToken, refreshToken, memberId] = parseCookie(cookieString);
+      console.log(accessToken);
       // 토큰을 로컬에 저장
       localStorage.setItem('accessToken', accessToken);
       localStorage.setItem('refreshToken', refreshToken);
---
+++ Frontend/src/types/userInfo.ts
@@ -0,0 +1,6 @@
+export type localUserInfo = {
+    accessToken: string;
+    refreshToken: string;
+    memberId: string;
+    nickname: string;
+};
